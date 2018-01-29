d0c04499edea9df88774799c1daff355ec8ba97e
Moming-Desgin <1192653484@qq.com>
2018-01-29 16:44:54 +0800

    20180129-android库R资源无法导入

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,11 +1,14 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+
+import com.example.javajoke.Joker;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,7 +43,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Joker joker = new Joker();
+        String joke = joker.getJoke();
+
+        Toast.makeText(this, joke, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, com.example.jokeandroidlib.MainActivity.class);
+        intent.putExtra("joke",joke);
+        startActivity(intent);
+
     }
 
 
---
+++ jokeandroidlib/src/main/java/com/example/jokeandroidlib/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.jokeandroidlib;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text_lib_joke);
+        Intent extra = getIntent();
+        String joke = extra.getStringExtra("joke");
+        textView.setText(joke);
+    }
+}
